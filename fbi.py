f867c2c117ec5e91d4dec6ee76d41c64f94bfef3
hack4lx <38162851+attakercyebr@users.noreply.github.com>
2021-03-30 09:00:03 +0000

    Older code does not work
    Older code does not work

--- fbi.py
+++ fbi.py
@@ -1,42 +1,4 @@
-# These module alos are used by protection code, so that protection
-# code needn't import anything
-import os
-import platform
-import sys
-import struct
-
-# Because ctypes is new from Python 2.5, so pytransform doesn't work
-# before Python 2.5
-#
-from ctypes import cdll, c_char, c_char_p, c_int, c_void_p, \
-    pythonapi, py_object, PYFUNCTYPE, CFUNCTYPE
-from fnmatch import fnmatch
-
-#
-# Support Platforms
-#
-plat_path = 'platforms'
-
-plat_table = (
-    ('windows', ('windows', 'cygwin-*')),
-    ('darwin', ('darwin',)),
-    ('ios', ('ios',)),
-    ('linux', ('linux*',)),
-    ('freebsd', ('freebsd*', 'openbsd*')),
-    ('poky', ('poky',)),
-)
-
-arch_table = (
-    ('x86', ('i?86', )),
-    ('x86_64', ('x64', 'x86_64', 'amd64', 'intel')),
-    ('arm', ('armv5',)),
-    ('armv6', ('armv6l',)),
-    ('armv7', ('armv7l',)),
-    ('ppc64', ('ppc64le',)),
-    ('mips32', ('mips',)),
-    ('aarch32', ('aarch32',)),
-    ('aarch64', ('aarch64', 'arm64'))
-)
+Older code does not work
 
 #
 # Hardware type
